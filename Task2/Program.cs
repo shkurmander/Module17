c362dda96e83f85a9110ad461c739cc3dccd9e8c
Vladimir Berdnikov <gazetter@mail.ru>
2021-01-20 23:06:27 +0700

    Написал инициализацию массива, метод для мечати массива, метод сортировки, класс исключения(пока не доделан)

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -9,29 +9,75 @@ namespace Task2
     /// Дополнительно реализуйте проверку введённых данных от пользователя через конструкцию TryCatchFinally с 
     /// использованием собственного типа исключения.
     /// </summary>
+    
     class Program
     {
+        /// <summary>
+        /// Метод сортирующий массив в порядке возрастания или убывания в зависимости от option
+        /// </summary>
+        /// <param name="array">Массив строк для сортировки</param>
+        /// <param name="option"> 1- сортировка по возрастанию; 2 - сортировка по убыванию </param>
+        /// <returns>Отсортированный массив array</returns>
+        public static string[] SortArray(string[] array, int option)
+        {
+            if (option == 1)
+            {
+                Array.Sort(array);
+                return array;
+            }
+            else
+            {
+                Array.Sort(array);
+                Array.Reverse(array);
+                return array;
+            }
+            
+        }
+        /// <summary>
+        /// Собственный класс исключения
+        /// </summary>
+        public class IncorrectActionException : Exception
+        {
+            public static string Message { get; set; }
+            public IncorrectActionException(string message, string val) : base(Message)
+            {
+                Message = message + "Полученный параметр: " + val ;
+            }
+        }
+        public static void PrintArray(string[] array)
+        {
+            Console.WriteLine("Массив:");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
 
         static void Main(string[] args)
         {
-            //string path = @"c:\test\students.dat";
-            string path = "";
-
+            
+            
+            string[] data = new string[5]
+            {
+                "Иванов",
+                "Петров",
+                "Алексеев",
+                "Сидоров",
+                "Борисов"
+            };
 
             bool exit = false;
             do
             {
-                //проверка на пустой ввод
-                do
+                try
                 {
-                    Console.WriteLine("введите путь к файлу с данными:");
-                    path = Console.ReadLine();
-                    if (path == "") Console.WriteLine("вы ничего не ввели!!!");
+                    Console.WriteLine();
+                }
+                catch (Exception)
+                {
 
-                } while (path.Length < 1);
-
-                
-
+                    throw;
+                }
                 Console.WriteLine("_________________________\n");
                 
                 Console.WriteLine("Для выхода нажмите ESC, для продолжения - любую клавишу");
