0ef20ee40278565124ff442519887e2727dec6d2
Vladimir Berdnikov <gazetter@mail.ru>
2021-01-20 22:28:02 +0700

    Задание 1 несколько не понятно условие, но реальизовал так как порекомендовали в слаке

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.IO;
 
 namespace Task1
 {
+    /// <summary>
+    /// Создайте свой тип исключения.
+    /// Сделайте массив из пяти различных видов исключений, включая собственный тип исключения.Реализуйте конструкцию TryCatchFinally,
+    /// в которой будет итерация на каждый тип исключения (блок finally по желанию).
+    /// В блоке catch выведите в консольном сообщении текст исключения.
+    /// </summary>
     class Program
     {
+        public class DevExeption : Exception
+        {
+            public DevExeption(string message) : base(message) { }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Exception[] exeptions = new Exception[5];
+
+            exeptions[0] = new DivideByZeroException("Делить на ноль нельзя!");
+            exeptions[1] = new StackOverflowException("Переполнение стека!");
+            exeptions[2] = new FileNotFoundException("Указанный файл не существует!");
+            exeptions[3] = new TimeoutException("Время ожидания операции истекло!");
+            exeptions[4] = new DevExeption("Исключение разработчика!");
+
+            foreach (var item in exeptions)
+            {
+                try
+                {
+                    throw item;
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task2
+{
+    /// <summary>
+    /// Создайте консольное приложение, в котором будет происходить 
+    /// сортировка списка фамилий из пяти человек. Сортировка должна происходить при помощи события. 
+    /// Сортировка происходит при введении пользователем либо числа 1 (сортировка А-Я), либо числа 2 (сортировка Я-А).
+    /// Дополнительно реализуйте проверку введённых данных от пользователя через конструкцию TryCatchFinally с 
+    /// использованием собственного типа исключения.
+    /// </summary>
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //string path = @"c:\test\students.dat";
+            string path = "";
+
+
+            bool exit = false;
+            do
+            {
+                //проверка на пустой ввод
+                do
+                {
+                    Console.WriteLine("введите путь к файлу с данными:");
+                    path = Console.ReadLine();
+                    if (path == "") Console.WriteLine("вы ничего не ввели!!!");
+
+                } while (path.Length < 1);
+
+                
+
+                Console.WriteLine("_________________________\n");
+                
+                Console.WriteLine("Для выхода нажмите ESC, для продолжения - любую клавишу");
+
+                if (Console.ReadKey().Key == ConsoleKey.Escape) exit = true;
+
+            } while (!exit);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
